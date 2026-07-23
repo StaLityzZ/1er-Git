7f8e24ae6e45cc88082ae437326886a5000f6c99
StaLityzZ <tuanander6@gmail.com>
2026-07-24 09:27:34 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import gsap from 'gsap';
-
 function Animation() {
-    const reseaux = document.querySelector('.reseaux');
-    
-    zone.addEventListener('mousemove', (event) => {
-        const rect = reseaux.getBoundingClientRect();
-        const x = gsap.utils.mapRange(rect.left, rect.right, -rect.width / 2, rect.width / 2, event.clientX);
-        const y = gsap.utils.mapRange(rect.top, rect.bottom, -rect.height / 2, rect.height / 2, event.clientY);
-        
-        gsap.to(".reseaux", {
-            x: x * strength,
-            y: y * strength,
-            duration: 0.4,
-            ease: 'power2.out',
-            overwrite: true
+    const strength = 0.4;
+
+    document.querySelectorAll('.reseaux').forEach((link) => {
+        link.addEventListener('mousemove', (event) => {
+            const rect = link.getBoundingClientRect();
+            const x = gsap.utils.mapRange(rect.left, rect.right, -rect.width / 2, rect.width / 2, event.clientX);
+            const y = gsap.utils.mapRange(rect.top, rect.bottom, -rect.height / 2, rect.height / 2, event.clientY);
+
+            gsap.to(link, {
+                x: x * strength,
+                y: y * strength,
+                duration: 0.4,
+                ease: 'power2.out',
+                overwrite: true
+            });
         });
-    });
-    
-    zone.addEventListener('mouseleave', () => {
-        gsap.to(".reseaux", {
-            x: 0,
-            y: 0,
-            duration: 0.7,
-            ease: 'elastic.out(1, 0.4)',
-            overwrite: true
+
+        link.addEventListener('mouseleave', () => {
+            gsap.to(link, {
+                x: 0,
+                y: 0,
+                duration: 0.7,
+                ease: 'elastic.out(1, 0.4)',
+                overwrite: true
+            });
         });
     });
 }
