46db32f0ccb32569ee5b0f539c6757d221cfaa94
StaLityzZ <tuanander6@gmail.com>
2026-07-24 08:39:48 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import gsap from 'gsap';
+
+gsap.to("main a img", {
+  backgroundColor: "red", // background-color
+  fontSize: 12, // font-size
+  boxShadow: "0px 0px 20px 20px red", // animate complex strings
+  borderRadius: "50% 50%",
+  height: "auto", // animate between auto and a px value 🪄
+});
