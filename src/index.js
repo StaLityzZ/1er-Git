9a159648f8329a91f966f99733aa6325a7aa7a1e
StaLityzZ <tuanander6@gmail.com>
2026-07-24 08:41:27 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import gsap from 'gsap';
 
-gsap.to("main a img", {
+gsap.to("img", {
   backgroundColor: "red", // background-color
   fontSize: 12, // font-size
   boxShadow: "0px 0px 20px 20px red", // animate complex strings
